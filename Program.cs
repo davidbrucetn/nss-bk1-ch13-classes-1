6d86c37a10915098a9406b9ac4bce627726dce17
davidbrucetn <davidbrucetn@gmail.com>
2020-08-26 14:16:21 -0500

    Chap 13 Explanations

--- Program.cs
+++ Program.cs
@@ -3,67 +3,6 @@
 namespace csharp_book1_chap13_classbasics
 {
 
-    public class Customer
-    {
-        // Public Properties
-        public string FirstName { get; set; }
-
-        public string LastName { get; set; }
-
-        public bool IsLocal { get; set; }
-
-        public string FullName
-        {
-            get
-            {
-                return $"{FirstName} {LastName}";
-            }
-        }
-    }
-
-    public class DeliveryService
-    {
-        /*
-          Properties
-        */
-        public string Name { get; set; }
-
-        public string TransitType { get; set; }
-
-        /*
-          Methods
-        */
-        public void Deliver(Product product, Customer customer)
-        {
-            Console.WriteLine($"Product delivered by {TransitType} to {customer.FullName}");
-        }
-    }
-
-    public class Product
-    {
-        /*
-          Properties
-        */
-        public string Title { get; set; }
-
-        public string Description { get; set; }
-
-        public double Price { get; set; }
-
-        public int Quantity { get; set; }
-
-        /*
-          Methods
-        */
-        public void Ship(Customer customer, DeliveryService service)
-        {
-            if (!customer.IsLocal)
-            {
-                service.Deliver(this, customer);
-            }
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
@@ -82,6 +21,31 @@ namespace csharp_book1_chap13_classbasics
             author.Prefix = "That's right; I said: ";
             author.Write(output);
             // --------------------------- User Defined Types - WRITER  ---------------------------------/
+
+            // --------------------------- Working with Product, Customer, DeliveryService ---------------------------------/
+            Product tinkerToys = new Product()
+            {
+                Title = "Tinker Toys",
+                Description = "You can build anything you want",
+                Price = 32.49,
+                Quantity = 25
+            };
+
+            Customer marcus = new Customer()
+            {
+                FirstName = "Marcus",
+                LastName = "Fulbright",
+                IsLocal = false
+            };
+
+            DeliveryService UPS = new DeliveryService()
+            {
+                Name = "UPS",
+                TransitType = "train"
+            };
+
+            // Ship the tinker toys to Marcus using UPS
+            tinkerToys.Ship(marcus, UPS);
 
         }
 
@@ -102,23 +66,5 @@ namespace csharp_book1_chap13_classbasics
         }
         // --------------------------- User Defined Types - WRITER ---------------------------------/
 
-        // --------------------------- Public Class - CUSTOMER ---------------------------------/
-        public class Customer
-        {
-            public string FirstName { get; set; }
-
-            public string LastName { get; set; }
-
-            public bool IsLocal { get; set; }
-
-            // Calculated property that has no setter. It is readonly.
-            public string FullName
-            {
-                get
-                {
-                    return $"{FirstName} {LastName}";
-                }
-            }
-        }
     }
 }
